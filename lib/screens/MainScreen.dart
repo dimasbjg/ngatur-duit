ab7ce033812ac4bf30b4477bf9b2ac0241df61f7
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2021-09-06 20:58:58 +0700

    creating simple widget

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -33,9 +32,105 @@ class _MainScreenState extends State<MainScreen> {
                 color: Colors.red,
               ),
               Container(
-                height: 100,
                 margin: EdgeInsets.all(10),
-                color: Colors.yellow,
+                padding: EdgeInsets.symmetric(vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.red[200],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(top: 10, bottom: 10),
+                      child: Text(
+                        "EXPENSE",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Icon(
+                              Icons.fastfood,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Icon(
+                              Icons.directions_car,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Icon(
+                              Icons.lightbulb,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Icon(
+                              Icons.wifi_tethering_rounded,
+                              color: Colors.white,
+                            ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Icon(
+                            Icons.attach_money,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      height: 60,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "0",
+                        ),
+                        keyboardType: TextInputType.number,
+                        style: TextStyle(fontSize: 40),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.yellow[300],
+                          onPrimary: Colors.red[300],
+                        ),
+                        child: Text("SAVE"),
+                      ),
+                    )
+                  ],
+                ),
               ),
               Container(
                 height: 100,
